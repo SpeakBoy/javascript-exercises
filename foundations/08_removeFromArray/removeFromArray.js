c1399c536717c6f0ba353ccad52eb1f6036e28c7
Nevin <nevinspeakman@outlook.com>
2026-07-27 12:10:27 -0400

    Complete 08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    const args = Array.prototype.slice.call(arguments, 1);
+
+    newArr = [];
+
+    for (item of arr)
+    {
+        if (!args.includes(item))
+        {
+            newArr.push(item);
+        }
+    }
+
+    return newArr;
 };
 
 // Do not edit below this line
