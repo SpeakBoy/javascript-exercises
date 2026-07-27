f93930d72e92b4283448de459bcfe9dc33c5d04f
Nevin <nevinspeakman@outlook.com>
2026-07-27 12:17:44 -0400

    Complete 09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b))
+    {
+        return "ERROR";
+    }
 
+    if (b < a)
+    {
+        lower = b;
+        upper = a;
+    }
+    else
+    {
+        lower = a;
+        upper = b;
+    }
+
+    let sum = 0;
+    for (let i = lower; i <= upper; i++)
+    {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
