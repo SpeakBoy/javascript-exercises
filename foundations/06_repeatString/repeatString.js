aa174d9eaee1a791fba763f7a190a17989b85a10
Nevin <nevinspeakman@outlook.com>
2026-07-27 11:58:58 -0400

    Complete 06_repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0)
+    {
+        return "ERROR";
+    }
+    
+    let repeatedString = "";
+    while (num > 0)
+    {
+        repeatedString = repeatedString.concat(string);
+        num--;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
