01ca350792eb97fb3c579197c26dfc6aed9ead5d
Nevin <nevinspeakman@outlook.com>
2026-07-29 11:22:16 -0400

    Complete 16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,8 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const thisYear = (new Date()).getFullYear();
+    return people.reduce((oldest, curr) => {
+        return ((curr.yearOfDeath ?? thisYear) - curr.yearOfBirth > (oldest.yearOfDeath ?? thisYear) - oldest.yearOfBirth) ? curr : oldest;
+    });
 };
 
 // Do not edit below this line
