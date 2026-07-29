92432688c51f9d1f6047dc72e6204f3b58db49e6
Nevin <nevinspeakman@outlook.com>
2026-07-29 10:57:04 -0400

    Complete 13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,31 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let left = 0;
+    let right = str.length - 1;
 
+    let strLowerCase = str.toLowerCase();
+
+    while (left < right)
+    {
+        while (!isAlphaNumeric(strLowerCase[left]))
+        {
+            left++;
+        }
+        while (!isAlphaNumeric(strLowerCase[right]))
+        {
+            right--;
+        }
+        if (strLowerCase[left] !== strLowerCase[right])
+            return false;
+        left++;
+        right--;
+    }
+    return true;
 };
+
+function isAlphaNumeric(ch)
+{
+    return /^[a-zA-Z0-9]+$/.test(ch);
+}
 
 // Do not edit below this line
 module.exports = palindromes;
