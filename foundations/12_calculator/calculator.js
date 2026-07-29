78ec37c4d5599218fb24529bab45dce9464378b9
Nevin <nevinspeakman@outlook.com>
2026-07-29 10:42:48 -0400

    Complete 12_calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	return nums.reduce((acc, curr) => add(acc, curr), 0);
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce((acc, curr) => acc * curr, 1);
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	let total = 1;
+  let curr = x;
+  while (curr > 1)
+  {
+    total *= curr;
+    curr--;
+  }
+  return total;
 };
 
 // Do not edit below this line
