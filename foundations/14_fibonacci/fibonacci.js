0f02a34f8726e68fcc25e0326b95b8b048800188
Nevin <nevinspeakman@outlook.com>
2026-07-29 11:07:12 -0400

    Complete 14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
+    let numX = +x;
+    if (numX < 0) return "OOPS";
+    let numOne = 0;
+    let numTwo = 1;
+    if (numX === 0) return numOne;
+    if (numX === 1) return numTwo;
 
+    let numThree;
+    let curr = 2;
+    while (curr <= numX)
+    {
+        numThree = numOne + numTwo;
+        numOne = numTwo;
+        numTwo = numThree;
+        curr++;
+    }
+
+    return numThree;
 };
 
 // Do not edit below this line
