21d3d9bc0262afbef0d96c834dfc5919396394b7
Nevin <nevinspeakman@outlook.com>
2026-09-23 13:52:03 -0400

    Complete 3_totalIntegers

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,5 +1,12 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function(obj) {
+    if (typeof obj !== "object" || obj === null) {
+        return;
+    }
+
+    const values = Object.values(obj);
+
+    return values.reduce((acc, curr) =>
+        Number.isInteger(curr) ? acc + 1 : acc + (totalIntegers(curr) ?? 0), 0);
 };
   
 // Do not edit below this line
