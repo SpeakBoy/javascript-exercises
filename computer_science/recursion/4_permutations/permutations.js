07b3ab0b34a9aef6a73fdfc5bc95e88e0e849688
Nevin <nevinspeakman@outlook.com>
2026-09-23 14:44:15 -0400

    Complete 4_permutations

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,5 +1,18 @@
-const permutations = function() {
-  
+const permutations = function(arr) {
+    if (arr.length <= 1) {
+        return [arr];
+    }
+
+    const currentLevelPerms = [];
+    for (let i = 0; i < arr.length; i++) {
+        const arrWithoutI = arr.toSpliced(i, 1);
+        const subLevelPerms = permutations(arrWithoutI);
+        for (const perm of subLevelPerms) {
+            currentLevelPerms.push([arr[i], ...perm]);
+        }   
+    }
+
+    return currentLevelPerms;
 };
   
 // Do not edit below this line
