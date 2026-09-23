d6ae2521268e19a61f8723788504dec43ed30fdb
Nevin <nevinspeakman@outlook.com>
2026-09-23 13:10:39 -0400

    Complete 2_contains

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,5 +1,17 @@
-const contains = function() {
-  
+const contains = function(obj, val) {
+    if (Object.is(obj, val)) {
+        return true;
+    }
+    if (obj !== Object(obj)) {
+        return false;
+    }
+
+    let currentLevelContains = false;
+    for (const objVal of Object.values(obj)) {
+        currentLevelContains ||= contains(objVal, val);
+    }
+
+    return currentLevelContains;
 };
   
 // Do not edit below this line
