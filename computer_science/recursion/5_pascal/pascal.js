7b7065bf2b4e1f2eaae185a4abd0c5f8416351c6
Nevin <nevinspeakman@outlook.com>
2026-09-23 14:56:37 -0400

    Complete 5_pascal

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,5 +1,15 @@
-const pascal = function() {
-  
+const pascal = function(n) {
+    if (n === 1) {
+        return [1];
+    }
+
+    const prevRow = [0, ...pascal(n-1), 0];
+    const currRow = [];
+    for (let i = 0; i < prevRow.length - 1; i++) {
+        currRow.push(prevRow[i] + prevRow[i + 1]);
+    }
+
+    return currRow;
 };
   
 // Do not edit below this line
